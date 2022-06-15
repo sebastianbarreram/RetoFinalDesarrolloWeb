42de05da3e9d8c5c5026896cd994edacf2dfff61
unknown <sbarrera.96@hotmail.com>
2022-06-15 09:01:24 -0500

    Create Spring Boot project

---
+++ src/main/java/com/sofkau/RetoFinalDesarrolloWeb/RetoFinalDesarrolloWebApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.RetoFinalDesarrolloWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoFinalDesarrolloWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoFinalDesarrolloWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkau/RetoFinalDesarrolloWeb/RetoFinalDesarrolloWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.RetoFinalDesarrolloWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoFinalDesarrolloWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
